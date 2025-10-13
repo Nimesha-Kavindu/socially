d8f4c738cfe2336baa96097599013b33bfa18454
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-10-13 12:01:56 +0530

    define the theme settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:socially/firebase_options.dart';
+import 'package:socially/utils/constants/colors.dart';
 import 'package:socially/views/responsive/moblie_layout.dart';
 import 'package:socially/views/responsive/responsive_layout.dart';
 import 'package:socially/views/responsive/web_layout.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +24,24 @@ class MyApp extends StatelessWidget {
       home: ResponsiveScreenLayout(
         mobileScreenLayout: MobileScreenLayout(),
         webScreenLayout: WebSceenLayout(),
+      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        brightness: Brightness.dark,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Colors.transparent,
+          selectedItemColor: mainOrangeColor,
+          unselectedItemColor: mainWhiteColor,
+        ),
+        snackBarTheme: SnackBarThemeData(
+          backgroundColor: mainOrangeColor,
+          contentTextStyle: TextStyle(
+            color: mainWhiteColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
       ),
     );
   }
